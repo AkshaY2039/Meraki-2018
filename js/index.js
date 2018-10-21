b534e94ee1b456b40db92e432d8b190c06f6f94e
shramonaroy <evd17i006@iiitdm.ac.in>
2018-10-21 05:54:04 +0530

    Added nav bar

--- js/index.js
+++ js/index.js
@@ -5,3 +5,22 @@ window.addEventListener('scroll', () => {
     children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
   }
 }, false)
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var sectionID = $(this.hash);
+      sectionID = sectionID.length ? sectionID : $('[name=' + this.hash.slice(1) + ']');
+
+      if (sectionID.length) {
+        $('html,body').animate({
+          scrollTop: sectionID.offset().top
+        }, 1000); // This animation is 1 second long - measured in milliseconds
+        
+        return false;
+      }
+    }
+  });
+});
+
